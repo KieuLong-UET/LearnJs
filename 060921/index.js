c0d03e7f8b42cbf266b08ffecb7a7a33384a5224
KieuLong-UET <kieunhatlong@gmail.com>
2021-09-06 18:02:06 +0700

    bat dong bo

---
+++ 060921/index.js
@@ -0,0 +1,13 @@
+const request = require('request');
+
+console.time('Time end');
+
+
+request('https://randomuser.me/api/', function (error, response, body) {
+//   console.error('error:', error); // Print the error if one occurred
+//   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+//   console.log('body:', body); // Print the HTML for the Google homepage.
+});
+
+
+console.timeEnd('Time end');
